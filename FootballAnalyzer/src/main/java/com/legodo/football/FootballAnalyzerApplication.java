d76c329442438bd5426942e6773d385f0774a548
vi_sc <vschmidt@opentext.com>
2017-11-16 11:17:54 +0100

    add initial project

---
+++ FootballAnalyzer/src/main/java/com/legodo/football/FootballAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.legodo.football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballAnalyzerApplication.class, args);
+	}
+}
