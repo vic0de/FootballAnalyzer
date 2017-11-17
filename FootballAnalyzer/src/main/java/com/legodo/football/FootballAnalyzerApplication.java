3483a8723a857e4634fe8b7b41bf53674f960724
vi_sc <vschmidt@opentext.com>
2017-11-17 18:17:24 +0100

    add Bean to fox Hystrix Stream

--- FootballAnalyzer/src/main/java/com/legodo/football/FootballAnalyzerApplication.java
+++ FootballAnalyzer/src/main/java/com/legodo/football/FootballAnalyzerApplication.java
@@ -2,9 +2,14 @@ package com.legodo.football;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 
 
@@ -12,10 +17,17 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableHystrixDashboard
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class FootballAnalyzerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FootballAnalyzerApplication.class, args);					
 	}
+	
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystrix.stream");
+    }
+	
 	
 }
